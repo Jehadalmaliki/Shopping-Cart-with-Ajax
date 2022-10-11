b0f156e64dcb4f54e47a5058683e5148ef364323
Jehad Almaliki <yourgitemail@example.com>
2022-10-11 21:39:44 +0300

    feat : migrate table

--- database/migrations/2022_10_11_183544_create_products_table.php
+++ database/migrations/2022_10_11_183544_create_products_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string("name", 255)->nullable();
+            $table->text("description")->nullable();
+            $table->string("image", 255)->nullable();
+            $table->decimal("price", 6, 2);
             $table->timestamps();
         });
     }
