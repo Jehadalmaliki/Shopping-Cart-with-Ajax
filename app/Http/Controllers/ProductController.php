90067bdb195794a35249d67f1711f1dac3f3cda3
Jehad Almaliki <yourgitemail@example.com>
2022-10-12 16:57:23 +0300

    new : create product controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    //
+    public function index()
+    {
+        $products = Product::all();
+        return view('products', compact('products'));
+    }
+   /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function cart()
+    {
+        return view('cart');
+    }
+}
