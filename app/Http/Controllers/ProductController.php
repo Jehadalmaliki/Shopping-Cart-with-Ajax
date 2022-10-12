9da99c41f1d0fd399f61bb9d05f96f45cfe16a9a
Jehad Almaliki <yourgitemail@example.com>
2022-10-12 17:07:26 +0300

    faet : add to cart

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -22,4 +22,31 @@ class ProductController extends Controller
     {
         return view('cart');
     }
+/**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    
+    public function addToCart($id)
+    {
+        $product = Product::findOrFail($id);
+
+        $cart = session()->get('cart', []);
+
+        if(isset($cart[$id])) {
+            $cart[$id]['quantity']++;
+        } else {
+            $cart[$id] = [
+                "name" => $product->name,
+                "quantity" => 1,
+                "price" => $product->price,
+                "image" => $product->image
+            ];
+        }
+
+        session()->put('cart', $cart);
+        return redirect()->back()->with('success', 'Product added to cart successfully!');
+    }
+
 }
